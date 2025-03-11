c3a453842ab67d0a8f97f357e6c87d7c56509cbd
EoTavinho <149340850+EoTavinho@users.noreply.github.com>
2025-03-11 14:28:58 -0300

    lista7

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Gustavo Henrique\Documents\GitHub\PSI\env
+set VIRTUAL_ENV=C:\Users\20221101110075\Documents\GitHub\PSI\env
 
 if not defined PROMPT set PROMPT=$P$G
 
