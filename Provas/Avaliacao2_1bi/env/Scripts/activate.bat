838a9073fe858c5795a5ef2131fd06d85f716f36
EoTavinho <149340850+EoTavinho@users.noreply.github.com>
2025-03-11 21:41:33 -0300

    .

--- Provas/Avaliacao2_1bi/env/Scripts/activate.bat
+++ Provas/Avaliacao2_1bi/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\20221101110075\Documents\GitHub\PSI\Provas\Avaliacao2_1bi\env
+set VIRTUAL_ENV=C:\Users\Gustavo Henrique\Documents\GitHub\PSI\Provas\Avaliacao2_1bi\env
 
 if not defined PROMPT set PROMPT=$P$G
 
