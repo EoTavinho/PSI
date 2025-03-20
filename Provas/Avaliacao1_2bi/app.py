bc4aa7e0c031e865e876c3bcf8d7babf811bc45b
EoTavinho <149340850+EoTavinho@users.noreply.github.com>
2025-03-19 21:24:14 -0300

    avaliacao 1 2bi

--- Provas/Avaliacao1_2bi/app.py
+++ Provas/Avaliacao1_2bi/app.py
@@ -17,16 +17,20 @@ def get_connection():
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/listar')
+def listar():
     #Pegando a variavel que faz conexão com o BD pela função get_connection (linha 10)
     conn = get_connection()
-
+    cursor = conn.cursor()
     #Usando (SELECT *) na tabela "users" do BD
     #Usando a função "fetchall" para pegar todas as colunas da tabela.
-    usuarios = conn.execute('SELECT * FROM usuarios').fetchall()
+    usuarios = cursor.execute('SELECT * FROM usuarios').fetchall()
     
     conn.close()
     
-    return render_template('index.html', usuarios = usuarios)
+    return render_template('listar.html', usuarios = usuarios)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -39,11 +43,12 @@ def register():
         else:
             # Se o usuario colocar o nome, o else vai rodar,  esse conn = get_connection() serve para representaro BD 
             conn = get_connection()
+            cursor = conn.cursor()
             #(INSERT INTO usuarios) => (Inserir na tabela usuarios)
             #qual coluna? => (nome)
             #qual o valor? => (?) por que não da pra colocar uma variavel do python direto no comando sqlite
             #logo depois do comando sqlite, coloca os valores em ordem respectiva para as interrogações
-            conn.execute('INSERT INTO usuarios(nome) VALUES(?)', (nome,))
+            cursor.execute('INSERT INTO usuarios (nome) VALUES(?)', (nome,))
             #commit para confirmar e salvar as alterações, igual github
             conn.commit()
             #fechar a conexão, sempre lembrar de fazer
@@ -52,3 +57,4 @@ def register():
         
     return render_template('register.html')
     
+
--- Provas/Avaliacao1_2bi/init_db.py
+++ Provas/Avaliacao1_2bi/init_db.py
@@ -2,13 +2,13 @@ import sqlite3
 
 # abre a conexão
 conn = sqlite3.connect('escola.db')
-
+cursor = conn.cursor()
 # localização do sql
 SCHEMA = 'schema.sql'
 
 # executa as declarações do sql para o banco
 with open(SCHEMA) as f:
-    conn.executescript(f.read())
+    cursor.executescript(f.read())
 
 # encerra as operações
 conn.commit()
