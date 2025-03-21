e536a4d18aad7ae4d7529f8801fe8b13e5050218
EoTavinho <149340850+EoTavinho@users.noreply.github.com>
2025-03-21 10:23:25 -0300

    prova finalizada

--- Provas/Avaliacao1_2bi/app.py
+++ Provas/Avaliacao1_2bi/app.py
@@ -9,8 +9,8 @@ def get_connection():
     # Colocando na variável "conn" a conexão com o banco
     # sqlite3.connect(DATABASE) abre (ou cria, se não existir) o banco de dados e retorna um objeto de conexão.
     conn = sqlite3.connect(DATABASE)
+    
     #sqlite3.Row permite acessar os resultados como se fossem dicionários, ou seja, os valores podem ser acessados tanto por índice quanto pelo nome da coluna.
-
     conn.row_factory = sqlite3.Row
 
     return conn
@@ -23,10 +23,9 @@ def index():
 def listar():
     #Pegando a variavel que faz conexão com o BD pela função get_connection (linha 10)
     conn = get_connection()
-    cursor = conn.cursor()
     #Usando (SELECT *) na tabela "users" do BD
     #Usando a função "fetchall" para pegar todas as colunas da tabela.
-    usuarios = cursor.execute('SELECT * FROM usuarios').fetchall()
+    usuarios = conn.execute('SELECT * FROM usuarios').fetchall()
     
     conn.close()
     
@@ -43,17 +42,16 @@ def register():
         else:
             # Se o usuario colocar o nome, o else vai rodar,  esse conn = get_connection() serve para representaro BD 
             conn = get_connection()
-            cursor = conn.cursor()
             #(INSERT INTO usuarios) => (Inserir na tabela usuarios)
             #qual coluna? => (nome)
             #qual o valor? => (?) por que não da pra colocar uma variavel do python direto no comando sqlite
             #logo depois do comando sqlite, coloca os valores em ordem respectiva para as interrogações
-            cursor.execute('INSERT INTO usuarios (nome) VALUES(?)', (nome,))
+            conn.execute('INSERT INTO usuarios (nome) VALUES(?)', (nome,))
             #commit para confirmar e salvar as alterações, igual github
             conn.commit()
             #fechar a conexão, sempre lembrar de fazer
             conn.close()
-            return redirect(url_for('index'))
+            return redirect(url_for('listar'))
         
     return render_template('register.html')
     
--- Provas/Avaliacao1_2bi/init_db.py
+++ Provas/Avaliacao1_2bi/init_db.py
@@ -2,13 +2,12 @@ import sqlite3
 
 # abre a conexão
 conn = sqlite3.connect('escola.db')
-cursor = conn.cursor()
 # localização do sql
 SCHEMA = 'schema.sql'
 
 # executa as declarações do sql para o banco
 with open(SCHEMA) as f:
-    cursor.executescript(f.read())
+    conn.executescript(f.read())
 
 # encerra as operações
 conn.commit()
